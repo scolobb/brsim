ba26a51e856e569e014f8b639b7a227da5389241
Sergiu Ivanov <sivanov@colimite.fr>
2014-04-17 17:15:49 +0300

    Add Rule.

--- ReactionSystems.hs
+++ ReactionSystems.hs
@@ -4,6 +4,18 @@ Defines reaction systems and some of their basic dynamics.-}
 
 module ReactionSystems ( Symbol(..)
                        , name
+                       , Symbols
                        ) where
 
+import qualified Data.Set as Set
+
 newtype Symbol = Symbol { name :: String } deriving (Show, Read, Ord, Eq)
+
+type Symbols = Set.Set Symbol
+
+data Rule = Rule { reactants :: Symbols
+                 , inhibitors :: Symbols
+                 , products :: Symbols
+                 } deriving (Show, Read, Ord, Eq)
+
+type Rules = Set.Set Rule
