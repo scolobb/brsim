c83e76cd884bd08faa3f4496359e8029ba948437
Sergiu Ivanov <sivanov@colimite.fr>
2014-04-21 14:25:48 +0300

    Modify the description of the commands to better fit the output.

--- brsim.hs
+++ brsim.hs
@@ -20,17 +20,17 @@ reactionFormat = Arg.Type { Arg.parser = \val -> case val of
                           }
 
 reactionFormatOpt = Arg.option ['f'] ["format"] reactionFormat Arrow
-                    "The format of the reaction description. \n\n\
-\The default value of this argument is \"arrow\", in which case the rules should be \
-\specified using a notation similar to the chemical notation:\n\n\
-\  <left hand side> -> <right hand side> | <list of inhibitors>\n\n\
-\For example, a reaction which consumes a and b, produces c and d, and is \
-\inhibited by e and f, can be written as follows:\n\n\
-\  a+b->c+d|e f\n\n\
-\If the value of this argument is \"plain\", the rules are specified as lists\
-\of reactants, inhibitors, and products respectively.  Thus, the same reaction\
-\can be written in the following way:\n\n\
-\  a b, e f, c d\n\n"
+                    "The format of the reaction description.\n\
+\    The default value of this argument is \"arrow\", in which case the rules should be\n\
+\    specified using a notation similar to the chemical notation:\n\n\
+\        <left hand side> -> <right hand side> | <list of inhibitors>\n\n\
+\    For example, a reaction which consumes a and b, produces c and d, and is inhibited\n\
+\    by e and f, can be written as follows:\n\n\
+\        a+b->c+d|e f\n\n\
+\    If the value of this argument is \"plain\", the rules are specified as lists of\n\
+\    reactants, inhibitors, and products respectively.  Thus, the same reaction can be\n\
+\    written in the following way:\n\n\
+\        a b, e f, c d"
 
 run = Command { name = "run"
               , action = withNonOption Arg.file $
@@ -38,13 +38,13 @@ run = Command { name = "run"
                          withOption reactionFormatOpt $
                          \format -> io $ do
                            putStrLn $ "Read " ++ show rsFile ++ " in format " ++ show format ++ "."
-              , description = "run <file> -- run the simulation of the reaction system\
-\given in <file>.  Relevant options: --format."
+              , description = "Run the simulation of the reaction system\
+\given in FILE.\n"
               }
 
 brsimCommand = Command { name = "brsim"
                        , action = io $ do
-                         putStrLn "No reaction system specified.\n\n"
+                         putStrLn "ERROR: No reaction system specified.  Showing usage information.\n"
                          showUsage brsimCommands
                        , description = "A Basic Reaction System Simulator."
                        }
