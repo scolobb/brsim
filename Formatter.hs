2cc44e5638ab831876f0c4a2219a1aea1a345603
Sergiu Ivanov <sivanov@colimite.fr>
2014-04-22 00:23:15 +0300

    Add the functions to show symbols.

--- Formatter.hs
+++ Formatter.hs
@@ -1,5 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
 {- Formatter.hs
 
 Pretty-prints the components of reaction systems to string.-}
 
 module Formatter () where
+
+import ReactionSystems
+import qualified Data.Text.Lazy as Text
+import qualified Data.Set as Set
+
+-- | Pretty-print the supplied symbol.
+showSymbol :: Symbol -> Text.Text
+showSymbol = Text.pack . name
+
+-- | Pretty-print the supplied set of symbol as a list.
+showSymbols :: Symbols -> Text.Text
+showSymbols = Text.intercalate " " . Set.toList . Set.map showSymbol
+
+-- | Pretty-print a list of lists of symbols.
+showListOfListsOfSymbols :: [Symbols] -> Text.Text
+showListOfListsOfSymbols = Text.unlines . map showSymbols
