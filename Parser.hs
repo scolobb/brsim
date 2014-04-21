fb988057b3d60aa6898bced79fb69f031e6f9b49
Sergiu Ivanov <sivanov@colimite.fr>
2014-04-21 15:09:23 +0300

    Add readSymbols and some handy shortcuts to it.

--- Parser.hs
+++ Parser.hs
@@ -1,5 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
 {- Parser.hs
 
 Reads the components of a reaction system from string.-}
 
-module Parser () where
+module Parser ( readSymbols
+              , readPlusSymbols
+              , readSpaceSymbols
+              ) where
+
+import ReactionSystems
+import qualified Data.Text as Text
+import qualified Data.Set as Set
+
+-- | Parses a list of symbols (second argument) separated by the given
+-- sequence of symbols (first argument).
+readSymbols :: Text.Text -> Text.Text -> Symbols
+readSymbols sep = Set.fromList . map (Symbol . Text.unpack) . filter (not . Text.null)
+                  . map Text.strip . Text.splitOn sep
+
+-- | Parses a plus-separated list of symbols.
+readPlusSymbols = readSymbols "+"
+
+-- | Parses a space-separated list of symbols.
+readSpaceSymbols = readSymbols " "
