1ee64662f2bb15be93226ed8fc98ce1da18cd2f1
Sergiu Ivanov <sivanov@colimite.fr>
2014-04-22 14:39:15 +0300

    Add annotatePlain and annotateArrow.

--- Formatter.hs
+++ Formatter.hs
@@ -9,6 +9,8 @@ module Formatter ( showSymbol
                  , showListOfListsOfSymbols
                  , showPlainReaction
                  , showArrowReaction
+                 , annotatePlain
+                 , annotateArrow
                  ) where
 
 import ReactionSystems
@@ -48,3 +50,27 @@ showArrowReaction (Reaction rcts inh prod) =
       txtProd = showPlusSymbols  prod
       txtInh  = showSpaceSymbols inh
   in txtRcts `Text.append` "->" `Text.append` txtProd `Text.append` "|" `Text.append` txtInh
+
+-- Annotate an interactive process of the supplied reaction system
+-- using a supplied reaction formatting function.
+annotate :: (Reaction -> Text.Text) -> ReactionSystem -> InteractiveProcess -> Text.Text
+annotate rformat (ReactionSystem _ rs) (InteractiveProcess contexts results) =
+  Text.unlines $ concatMap annotateState $ zip3 [1..] contexts results
+  where annotateState (n, ctx, res) =
+          let state = (ctx `Set.union` res)
+          in [ "STEP " `Text.append` (Text.pack $ show res)
+             , "Context:    " `Text.append` showSpaceSymbols ctx
+             , "Last result:" `Text.append` showSpaceSymbols res
+             , "State:      " `Text.append` showSpaceSymbols state
+             , "Enabled rules:"
+             ] ++ (map (Text.append "  " . rformat) $ filter ((flip enabled) state) $ Set.toList rs)
+
+-- | Annotate an interactive process of the supplied reaction system,
+-- printing rules in plain format.
+annotatePlain :: ReactionSystem -> InteractiveProcess -> Text.Text
+annotatePlain = annotate showPlainReaction
+
+-- | Annotate an interactive process of the supplied reaction system,
+-- printing rules in arrow format.
+annotateArrow :: ReactionSystem -> InteractiveProcess -> Text.Text
+annotateArrow = annotate showArrowReaction
